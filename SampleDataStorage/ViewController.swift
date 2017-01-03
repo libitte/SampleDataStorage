1d11bdf7577f99a0363b4d3b19d7c8316f22c2a0
Nobuto HACHIYA <libitte3@gmail.com>
2017-01-03 21:11:09 +0900

    Initial Commit

---
+++ SampleDataStorage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleDataStorage
+//
+//  Created by Nobuto Hachiya on 2017/01/03.
+//  Copyright © 2017年 Nobuto Hachiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
